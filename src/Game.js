794ba007888dae3c0ec698017bbacc39a76b2221
Nikko <nikkos_12@hotmail.com>
2013-10-08 22:19:45 +0200

    Added tests and refactored code

--- src/Game.js
+++ src/Game.js
@@ -20,11 +20,19 @@ Game.prototype.getPlayerTwoName = function () {
 }
 
 Game.prototype.getScore = function () {
-	if (this._playerOneScore == 1 && this._playerTwoScore == 0) {
-		return "15 - 0";
+	if (this.hasWinner()) {
+		return this.playerWithHighestScore() + " has won the game !";
 	}
 	
-	return "0 - 0";
+	if (this.hasAdvantage()) {
+		return "Advantage " + this.playerWithHighestScore();
+	}
+	
+	if (this.isDeuce()) {
+		return "Deuce";
+	}
+	
+	return this.translateScore(this._playerOneScore) + " - " + this.translateScore(this._playerTwoScore);
 };
 
 Game.prototype.playerOneScores = function () {
@@ -32,4 +40,51 @@ Game.prototype.playerOneScores = function () {
 }
 Game.prototype.playerTwoScores = function () {
 	this._playerTwoScore++;
+}
+
+Game.prototype.isDeuce = function () {
+	return (this._playerOneScore >= 3 && this._playerTwoScore == this._playerOneScore);
+}
+
+Game.prototype.playerWithHighestScore = function () {
+	return (this._playerOneScore > this._playerTwoScore) ? this._playerOneNamer : this._playerTwoName;
+}
+
+Game.prototype.hasAdvantage = function () {
+	if (this._playerOneScore >= 4 && this._playerOneScore === this._playerTwoScore + 1) {
+		return true;
+	}
+	
+	if (this._playerTwoScore >= 4 && this._playerTwoScore === this._playerOneScore + 1) {
+		return true;
+	}
+	
+	return false;
+}
+
+Game.prototype.hasWinner = function () {
+	if (this._playerOneScore >= 4 && this._playerOneScore === this._playerTwoScore + 2) {
+		return true;
+	}
+	
+	if (this._playerTwoScore >= 4 && this._playerTwoScore === this._playerOneScore + 2) {
+		return true;
+	}
+	
+	return false;
+}
+
+Game.prototype.translateScore = function (score) {
+	switch (score) {
+		case 0:
+			return "0";
+		case 1:
+			return "15";
+		case 2:
+			return "30";
+		case 3:
+			return "40";
+		default:
+			"Dat game is CRAZYYY ! Dat score doesn't even exist !";
+	}
 }
