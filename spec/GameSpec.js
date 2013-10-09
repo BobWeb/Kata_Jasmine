8b8f5dccfe02b4242728d623c33f571e11af2e98
Nicolas DOVRAN <nicolas.dovran@gmail.com>
2013-10-09 18:01:43 +0200

    Ajout de novueaux tests
    Ajout de nouveaux tests et correctif de la méthode
    playerWithHighestScore

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,70 +1,78 @@
 game = new Game();
 
 describe('Un match de tennis en simple', function () {
-	beforeEach(function () {  
-		this.addMatchers({  
-			toBeBetween: function (rangeFloor, rangeCeiling) {  
-				if (rangeFloor > rangeCeiling) {  
-					var temp = rangeFloor;  
-					rangeFloor = rangeCeiling;  
-					rangeCeiling = temp;  
-				}  
-				return this.actual > rangeFloor && this.actual < rangeCeiling;  
-			}  
-		});
-	});  
-	it('Deux joueurs entrent sur le terrain', function() {
-		expect(game._playerOneName).toBeDefined();
-		expect(game._playerTwoName).toBeDefined();
-		expect(game._playerOneScore).toBeDefined();
-		expect(game._playerTwoScore).toBeDefined();
-	});
-	it('Le premier joueur est Federer', function() {
-		game.setPlayerOneName('Federer');
+    /*beforeEach(function () {
+        this.addMatchers({
+            toBeBetween: function (rangeFloor, rangeCeiling) {
+                if (rangeFloor > rangeCeiling) {
+                    var temp = rangeFloor;
+                    rangeFloor = rangeCeiling;
+                    rangeCeiling = temp;
+                }
+                return this.actual > rangeFloor && this.actual < rangeCeiling;
+            }
+        });
+    });*/
+    it('Deux joueurs entrent sur le terrain', function() {
+        expect(game._playerOneName).toBeDefined();
+        expect(game._playerTwoName).toBeDefined();
+        expect(game._playerOneScore).toBeDefined();
+        expect(game._playerTwoScore).toBeDefined();
+    });
+    it('Le premier joueur est Federer', function() {
+        game.setPlayerOneName('Federer');
         expect(game.getPlayerOneName()).toEqual('Federer');
     });
-	it('Le second joueur est Djokovic', function() {
-		game.setPlayerTwoName('Djokovic');
+    it('Le second joueur est Djokovic', function() {
+        game.setPlayerTwoName('Djokovic');
         expect(game.getPlayerTwoName()).toContain('Djoko');
     });
-	it('Le score est vierge', function() {
+    it('Le score est vierge', function() {
         expect(game.getScore()).toEqual('0 - 0');
     });
-	it('Federer fait un ace !', function() {
-		game.playerOneScores();
+    it('Federer fait un ace !', function() {
+        game.playerOneScores();
         expect(game._playerOneScore).toEqual(1);
-		expect(game.getScore()).toEqual('15 - 0');
+        expect(game.getScore()).toEqual('15 - 0');
     });
-	it('Federer score de nouveau !', function() {
-		game.playerOneScores();
+    it('Federer score de nouveau !', function() {
+        game.playerOneScores();
         expect(game._playerOneScore).toEqual(2);
-		expect(game.getScore()).toEqual('30 - 0');
+        expect(game.getScore()).toEqual('30 - 0');
     });
-	it('Djokovic réduit le score', function() {
-		game.playerTwoScores();
+    it('Djokovic réduit le score', function() {
+        game.playerTwoScores();
         expect(game._playerTwoScore).toEqual(1);
-		expect(game.getScore()).toEqual('30 - 15');
+        expect(game.getScore()).toEqual('30 - 15');
     });
-	it('Djokovic égalise !', function() {
-		game.playerTwoScores();
+    it('Djokovic égalise !', function() {
+        game.playerTwoScores();
         expect(game._playerTwoScore).toEqual(2);
-		expect(game.getScore()).toEqual('30 - 30');
+        expect(game.getScore()).toEqual('30 - 30');
     });
-	it('Balle de match Djoko !!', function() {
-		game.playerTwoScores();
+    it('Balle de match Djoko !!', function() {
+        game.playerTwoScores();
         expect(game._playerTwoScore).toEqual(3);
-		expect(game.getScore()).toEqual('30 - 40');
+        expect(game.getScore()).toEqual('30 - 40');
     });
-	it('Egalité !!', function() {
-		game.playerOneScores();
+    it('Egalité !!', function() {
+        game.playerOneScores();
         expect(game._playerOneScore).toEqual(3);
-		expect(game._playerTwoScore).toEqual(game._playerTwoScore);
-		expect(game.getScore()).toEqual('Deuce');
-		expect(game._playerOneScore).toBeGreaterThan(2);
+        expect(game._playerTwoScore).toEqual(game._playerTwoScore);
+        expect(game.getScore()).toEqual('Deuce');
+        expect(game._playerOneScore).toBeGreaterThan(2);
     });
-	it('Le score est "deuce"', function() {
+    it('Le score est "deuce"', function() {
         spyOn(game, 'isDeuce');
-		game.getScore();
-		expect(game.isDeuce).toHaveBeenCalled();
+        game.getScore();
+        expect(game.isDeuce).toHaveBeenCalled();
+    });
+    it('Avantage Federer !', function() {
+        game.playerOneScores();
+        expect(game.getScore()).toEqual('Advantage Federer');
+    });
+    it('Federer l\'emporte !', function() {
+        game.playerOneScores();
+        expect(game.getScore()).toEqual('Federer has won the game !');
     });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,8 @@
 var Game = function () {
-	this._playerOneName  = '';
-	this._playerTwoName  = '';
-	this._playerOneScore = 0;
-	this._playerTwoScore = 0;
+    this._playerOneName  = '';
+    this._playerTwoName  = '';
+    this._playerOneScore = 0;
+    this._playerTwoScore = 0;
 }
 
 Game.prototype.setPlayerOneName = function (name) {
@@ -20,71 +20,71 @@ Game.prototype.getPlayerTwoName = function () {
 }
 
 Game.prototype.getScore = function () {
-	if (this.hasWinner()) {
-		return this.playerWithHighestScore() + " has won the game !";
-	}
-	
-	if (this.hasAdvantage()) {
-		return "Advantage " + this.playerWithHighestScore();
-	}
-	
-	if (this.isDeuce()) {
-		return "Deuce";
-	}
-	
-	return this.translateScore(this._playerOneScore) + " - " + this.translateScore(this._playerTwoScore);
+    if (this.hasWinner()) {
+        return this.playerWithHighestScore() + " has won the game !";
+    }
+
+    if (this.hasAdvantage()) {
+        return "Advantage " + this.playerWithHighestScore();
+    }
+
+    if (this.isDeuce()) {
+        return "Deuce";
+    }
+
+    return this.translateScore(this._playerOneScore) + " - " + this.translateScore(this._playerTwoScore);
 };
 
 Game.prototype.playerOneScores = function () {
-	this._playerOneScore++;
+    this._playerOneScore++;
 }
 Game.prototype.playerTwoScores = function () {
-	this._playerTwoScore++;
+    this._playerTwoScore++;
 }
 
 Game.prototype.isDeuce = function () {
-	return (this._playerOneScore >= 3 && this._playerTwoScore == this._playerOneScore);
+    return (this._playerOneScore >= 3 && this._playerTwoScore == this._playerOneScore);
 }
 
 Game.prototype.playerWithHighestScore = function () {
-	return (this._playerOneScore > this._playerTwoScore) ? this._playerOneNamer : this._playerTwoName;
+    return (this._playerOneScore > this._playerTwoScore) ? this._playerOneName : this._playerTwoName;
 }
 
 Game.prototype.hasAdvantage = function () {
-	if (this._playerOneScore >= 4 && this._playerOneScore === this._playerTwoScore + 1) {
-		return true;
-	}
-	
-	if (this._playerTwoScore >= 4 && this._playerTwoScore === this._playerOneScore + 1) {
-		return true;
-	}
-	
-	return false;
+    if (this._playerOneScore >= 4 && this._playerOneScore === this._playerTwoScore + 1) {
+        return true;
+    }
+
+    if (this._playerTwoScore >= 4 && this._playerTwoScore === this._playerOneScore + 1) {
+        return true;
+    }
+
+    return false;
 }
 
 Game.prototype.hasWinner = function () {
-	if (this._playerOneScore >= 4 && this._playerOneScore === this._playerTwoScore + 2) {
-		return true;
-	}
-	
-	if (this._playerTwoScore >= 4 && this._playerTwoScore === this._playerOneScore + 2) {
-		return true;
-	}
-	
-	return false;
+    if (this._playerOneScore >= 4 && this._playerOneScore === this._playerTwoScore + 2) {
+        return true;
+    }
+
+    if (this._playerTwoScore >= 4 && this._playerTwoScore === this._playerOneScore + 2) {
+        return true;
+    }
+
+    return false;
 }
 
 Game.prototype.translateScore = function (score) {
-	switch (score) {
-		case 0:
-			return "0";
-		case 1:
-			return "15";
-		case 2:
-			return "30";
-		case 3:
-			return "40";
-		default:
-			"Dat game is CRAZYYY ! Dat score doesn't even exist !";
-	}
+    switch (score) {
+        case 0:
+            return "0";
+        case 1:
+            return "15";
+        case 2:
+            return "30";
+        case 3:
+            return "40";
+        default:
+            "Dat game is CRAZYYY ! Dat score doesn't even exist !";
+    }
 }
